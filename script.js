5ee9e15f45335250279d5c3353e68e8cad558504
IG-Kim <iikim2511@gmail.com>
2020-09-26 13:36:49 -0400

    no message

---
+++ script.js
@@ -0,0 +1,71 @@
+//● 즉시실행 함수 Immediately-invoked function expression
+// 전역변수를 안쓰기위해서 씀
+// 함수안에서 만든 변수...밖에서 쓸수없음
+
+(() => {
+  //●js 02-03 : `data-index=''` 넣기 to .step , .graphic-item
+
+  const stepElems = document.querySelectorAll(".step");
+
+  const graphicElems = document.querySelectorAll(".graphic-item");
+
+  let currentItem = graphicElems[0];
+
+  for (let i = 0; i < stepElems.length; i++) {
+    // console.log(stepElems[i]);
+
+    // stepElems[i].setAttribute('data-index',i);   <--같은뜻
+
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
+
+  //● js 02-08: 말풍선 어느정도 높이 도달할때, 뒤의 이미지 바꾸기
+  // getboundingclientrect :  https://iankim2511.tistory.com/13
+  // getBoundingClientRect 엘리먼트 위치값 알아냄. y값. top값
+  // (4) 윈도우높이의 10%~80% 사이에서 말풍선,이미지 호출 : step의 dataset의 index호출
+  //(5) step의 dataset의 index 확인
+  // (6) 스크롤에따라 이미지 호출하기  : 활성화된 이미지를 currentItem에 넣고, currentItem을 지우고, 다음 이미지를 호출함
+  // (7)  currentItem 첫image로 시작
+
+  window.addEventListener("scroll", () => {
+    let step;
+    let boundingRect;
+
+    for (let i = 0; i < stepElems.length; i++) {
+      step = stepElems[i];
+      boundingRect = step.getBoundingClientRect();
+
+      // console.log(boundingRect);
+      // console.log(boundingRect.top);
+
+      // (4)
+      if (
+        boundingRect.top > window.innerHeight * 0.1 &&
+        boundingRect.top < window.innerHeight * 0.8
+      ) {
+        // (5)
+        // console.log(step.dataset.index);
+
+        // (6)
+        if (currentItem) {
+          currentItem.classList.remove("visible");
+        }
+
+        currentItem = graphicElems[step.dataset.index];
+        currentItem.classList.add("visible");
+      }
+    }
+  });
+
+  // (7)
+  currentItem.classList.add("visible");
+
+  //● js 03-21 새로고침하면 최상단 첫화면
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      scrollTo(0, 0);
+    }, 100);
+  });
+})();
