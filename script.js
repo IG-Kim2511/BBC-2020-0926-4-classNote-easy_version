dfd4ea73d700397d66d71c5a89fa3c029ae4182b
IG-Kim <iikim2511@gmail.com>
2020-09-28 01:25:53 -0400

    ..

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
   const graphicElems = document.querySelectorAll(".graphic-item");
 
+  // (8) 첫화면에 이미지 load
   let currentItem = graphicElems[0];
 
   for (let i = 0; i < stepElems.length; i++) {
@@ -22,12 +23,33 @@
 
   //● js 02-08: 말풍선 어느정도 높이 도달할때, 뒤의 이미지 바꾸기
   // getboundingclientrect :  https://iankim2511.tistory.com/13
-  // getBoundingClientRect 엘리먼트 위치값 알아냄. y값. top값
+  // (3) getBoundingClientRect 엘리먼트 위치값 알아냄. y값. top값
   // (4) 윈도우높이의 10%~80% 사이에서 말풍선,이미지 호출 : step의 dataset의 index호출
   //(5) step의 dataset의 index 확인
   // (6) 스크롤에따라 이미지 호출하기  : 활성화된 이미지를 currentItem에 넣고, currentItem을 지우고, 다음 이미지를 호출함
   // (7) 중복코딩 function으로 만듬
   // (8)  currentItem 첫image로 시작
+
+  // <쉬운버전>
+  // window.addEventListener("scroll", () => {
+  //   let step;
+  //   let boundingRect;
+
+  //   for (let i = 0; i < stepElems.length; i++) {
+  //     step = stepElems[i];
+  //     boundingRect = step.getBoundingClientRect();
+
+  //       if (
+  //       boundingRect.top > window.innerHeight * 0.1 &&
+  //       boundingRect.top < window.innerHeight * 0.8
+  //     ) {
+  //       graphicElems[step.dataset.index].classList.add("visible");
+  //     } else {
+  //       graphicElems[step.dataset.index].classList.remove("visible");
+  //     }
+  //   }
+  // });
+  //   graphicElems[0].classList.add("visible");
 
   window.addEventListener("scroll", () => {
     let step;
@@ -37,6 +59,7 @@
       step = stepElems[i];
       boundingRect = step.getBoundingClientRect();
 
+      // (3)
       // console.log(boundingRect);
       // console.log(boundingRect.top);
 
@@ -53,6 +76,7 @@
           inactivate();
         }
 
+        // graphicElems[step.dataset.index].classList.add('visible');
         currentItem = graphicElems[step.dataset.index];
         activate();
       }
@@ -67,10 +91,11 @@
     currentItem.classList.remove("visible");
   }
 
-  // (8)
+  // (8) 첫화면에 이미지 load
   activate();
 
   //● js 03-21 새로고침하면 최상단 첫화면
+  // T: setTimeout / scrollTo
 
   window.addEventListener("load", () => {
     setTimeout(() => {
